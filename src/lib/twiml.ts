b37b84dba1b24860832ee28f5a9fb8838602eeaf
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-03-12 17:00:43 +0900

    fix length

--- src/lib/twiml.ts
+++ src/lib/twiml.ts
@@ -2,7 +2,8 @@ import { NextApiResponse } from "next"
 import VoiceResponse, { SayAttributes } from "twilio/lib/twiml/VoiceResponse"
 
 export const DEFAULT_SAY_ATTRIBUTE: SayAttributes = { voice: "man", language: "ja-JP" }
-export const GATHER_DIGIT_LENGTH = 8
+export const GATHER_DIGIT_LENGTH = 9
+
 export const responseTwiml = (res: NextApiResponse, twiml: VoiceResponse) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/xml')
